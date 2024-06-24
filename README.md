b969e913c6ff120fc39c3da12681e44814bf4d06
matoslucas <matoslucas@hotmail.com>
2024-06-24 08:06:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# copa-america-2024
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/matoslucas/copa-america-2024)
---
+++ index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+console.log('Version 0.0.1');
+createRoot(document.getElementById('container')!).render(<App />);
