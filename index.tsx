a76152d3c28c6fbd38efc9440157218cf7fbc65a
Lucas Matos <matoslucas@hotmail.com>
2024-06-26 20:11:49 -0500

    Update

--- index.tsx
+++ index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-console.log('Version 0.0.1');
+console.log('Version 0.0.01');
 createRoot(document.getElementById('container')!).render(<App />);
